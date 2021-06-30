9bd518fbbc9e3721cb542331f276682eca0963a6
Aleksandrs Barkancevs <barkancev.alexander@gmail.com>
2021-07-01 02:43:52 +0300

    Добавлен притиер файл

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { Component } from "react";
-import { Route, NavLink, Switch } from "react-router-dom";
+import { Component } from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom';
 // import styles from "./App.module.css";
-import HomeView from "./views/HomeView";
-import AuthorsView from "./views/AuthorsView";
-import BooksView from "./views/BooksView";
-import NotFoundView from "./views/NotFoundView";
+import HomeView from './views/HomeView';
+import AuthorsView from './views/AuthorsView';
+import BooksView from './views/BooksView';
+import NotFoundView from './views/NotFoundView';
+
+const API_KEY = 'dc54b28984a60b0de8f79c9322eebf59';
+const Example_API_Request =
+  'https://api.themoviedb.org/3/movie/550?api_key=dc54b28984a60b0de8f79c9322eebf59';
+const READ_ACC_TOKEN =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzU0YjI4OTg0YTYwYjBkZThmNzljOTMyMmVlYmY1OSIsInN1YiI6IjYwZGNmZDBkOTk3OWQyMDAyOTQzNmJhZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gZolqivUSBHzbWdsqpPOVtBSN2X05rqq4rm1n9fSMtE';
 
 class App extends Component {
   render() {
@@ -12,13 +18,13 @@ class App extends Component {
       <>
         <ul>
           <li>
-            <NavLink to="/">Home</NavLink>{" "}
+            <NavLink to="/">Home</NavLink>{' '}
           </li>
           <li>
-            <NavLink to="/authors">Authors</NavLink>{" "}
+            <NavLink to="/authors">Authors</NavLink>{' '}
           </li>
           <li>
-            <NavLink to="/books">Books</NavLink>{" "}
+            <NavLink to="/books">Books</NavLink>{' '}
           </li>
         </ul>
         <Switch>
